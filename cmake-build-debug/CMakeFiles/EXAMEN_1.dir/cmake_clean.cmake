c9f36decee5833c4637234d74cf22e83da9794d3
Morphos-afk <jbalbsan@myuax.com>
2024-11-05 14:31:45 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/EXAMEN_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EXAMEN_1.dir/main.cpp.obj"
+  "CMakeFiles/EXAMEN_1.dir/main.cpp.obj.d"
+  "EXAMEN_1.exe"
+  "EXAMEN_1.exe.manifest"
+  "EXAMEN_1.pdb"
+  "libEXAMEN_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EXAMEN_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
